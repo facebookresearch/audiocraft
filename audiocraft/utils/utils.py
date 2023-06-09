a8ec0d007f1c3680a0ff646534896d785a32213b
Alexandre Défossez <alexandre.defossez@gmail.com>
2023-06-09 12:30:08 +0300

    fix topp

--- audiocraft/utils/utils.py
+++ audiocraft/utils/utils.py
@@ -122,7 +122,7 @@ def sample_top_p(probs: torch.Tensor, p: float) -> torch.Tensor:
     probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
     probs_sum = torch.cumsum(probs_sort, dim=-1)
     mask = probs_sum - probs_sort > p
-    probs_sort *= (~mask).float(0)
+    probs_sort *= (~mask).float()
     probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
     next_token = multinomial(probs_sort, num_samples=1)
     next_token = torch.gather(probs_idx, -1, next_token)
