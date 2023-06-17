36b51baa0c610197a0b814291e2d5349a320fb64
Aditya <as904465@gmail.com>
2023-06-18 01:56:00 +0530

    Add function for saving audio samples to local storage

--- audiocraft/utils/notebook.py
+++ audiocraft/utils/notebook.py
@@ -30,3 +30,25 @@ def display_audio(samples: torch.Tensor, sample_rate: int):
 
     for audio in samples:
         ipd.display(ipd.Audio(audio, rate=sample_rate))
+
+
+import soundfile as sf
+
+def save_audio(samples: torch.Tensor, sample_rate: int, filepath: str):
+    """Saves the audio samples to a file.
+
+    Args:
+        samples (torch.Tensor): a Tensor of decoded audio samples
+            with shapes [B, C, T] or [C, T]
+        sample_rate (int): sample rate of the audio
+        filepath (str): path to save the audio file
+    """
+    assert samples.dim() == 2 or samples.dim() == 3
+
+    samples = samples.detach().cpu()
+    if samples.dim() == 2:
+        samples = samples[None, ...]
+
+    for i, audio in enumerate(samples):
+        audio = audio.numpy().T  # Transpose audio for saving
+        sf.write(filepath.format(i), audio, sample_rate)
