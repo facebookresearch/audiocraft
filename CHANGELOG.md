207888c76af1ed8ccbae0939b59ace6e69b14803
Alexandre Défossez <alexandre.defossez@gmail.com>
2023-08-01 20:17:22 +0200

    v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
-## [0.0.2a] - TBD
+## [0.0.2] - 2023-08-01
 
 Improved demo, fixed top p (thanks @jnordberg).
 
--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ from setuptools import setup, find_packages
 NAME = 'audiocraft'
 DESCRIPTION = 'Audio research library for PyTorch'
 
-URL = 'https://github.com/fairinternal/audiocraft'
+URL = 'https://github.com/facebookresearch/audiocraft'
 AUTHOR = 'FAIR Speech & Audio'
-EMAIL = 'defossez@meta.com'
+EMAIL = 'defossez@meta.com, jadecopet@meta.com'
 REQUIRES_PYTHON = '>=3.8.0'
 
 for line in open('audiocraft/__init__.py'):
