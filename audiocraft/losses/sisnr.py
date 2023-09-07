5e935bc34c14b39862eb13a6671c3bd6e76cd8da
Alexandre Défossez <alexandre.defossez@gmail.com>
2023-09-07 14:37:39 +0200

    making doc clearer

--- audiocraft/losses/sisnr.py
+++ audiocraft/losses/sisnr.py
@@ -41,8 +41,9 @@ class SISNR(nn.Module):
 
     Input should be [B, C, T], output is scalar.
 
-    ..Warning:: We report here minus SISNR, so negative values are better. This is so that
-        this can be naturally used as a loss!
+    ..Warning:: This function returns the opposite of the SI-SNR (e.g. `-1 * regular_SI_SNR`). Consequently, lower scores are
+        better in terms of reconstruction quality, in particular, it should be negative if training
+        goes well. This done this way so that this module can also be used as a loss function for training model.
 
     Args:
         sample_rate (int): Sample rate.
