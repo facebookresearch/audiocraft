1e64c01ed5d1236c6953fb68b2b5bbab83c397a1
Alexandre Défossez <alexandre.defossez@gmail.com>
2023-09-07 15:33:30 +0200

    line too long

--- audiocraft/losses/sisnr.py
+++ audiocraft/losses/sisnr.py
@@ -41,9 +41,10 @@ class SISNR(nn.Module):
 
     Input should be [B, C, T], output is scalar.
 
-    ..Warning:: This function returns the opposite of the SI-SNR (e.g. `-1 * regular_SI_SNR`). Consequently, lower scores are
-        better in terms of reconstruction quality, in particular, it should be negative if training
-        goes well. This done this way so that this module can also be used as a loss function for training model.
+    ..Warning:: This function returns the opposite of the SI-SNR (e.g. `-1 * regular_SI_SNR`).
+        Consequently, lower scores are better in terms of reconstruction quality,
+        in particular, it should be negative if training goes well. This done this way so
+        that this module can also be used as a loss function for training model.
 
     Args:
         sample_rate (int): Sample rate.
